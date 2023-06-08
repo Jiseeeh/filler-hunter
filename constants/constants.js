d10e93aeeed54718d14a17bc9302628dce56e56d
Jiseeeh <johncarlo.camara1@gmail.com>
2023-06-08 23:22:34 +0800

    feat: add constants file

---
+++ constants/constants.js
@@ -0,0 +1,7 @@
+export const URL = "https://www.animefillerlist.com";
+export const AXIOS_OPTIONS = {
+  headers: {
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+  },
+};
