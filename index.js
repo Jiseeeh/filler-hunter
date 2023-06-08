0515b54620c00a9f05eefcf29c32eb66d295bce8
Jiseeeh <johncarlo.camara1@gmail.com>
2023-06-08 23:22:55 +0800

    refractor: extract to helper fns

--- index.js
+++ index.js
@@ -1,39 +1,11 @@
-import axios from "axios";
-import { load } from "cheerio";
-import prompt from "prompt-sync";
+import getFillers from "./helper/getFillers.js";
+import getUserInput from "./helper/getUserInput.js";
+import getAnimeList from "./helper/getAnimeList.js";
 
-const input = prompt({ sigint: true });
-
-// get target site
-const URL = "https://www.animefillerlist.com";
-const options = {
-  headers: {
-    "User-Agent":
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-  },
-};
-
-const showsPage = await axios.get(`${URL}/shows`, options);
-
-const $ = load(showsPage.data);
-
-const animeList = [];
-
-$("#ShowList")
-  .find(".Group")
-  .each((_, el) => {
-    $(el)
-      .find("ul li")
-      .each((_, el) => {
-        const title = $(el).find("a").text().toLowerCase();
-        const path = $(el).find("a").attr("href");
-
-        animeList.push({ title, path });
-      });
-  });
+const animeList = getAnimeList();
 
 // get user input
-let query = input("Enter anime title to get its fillers: ");
+let query = getUserInput();
 
 const anime = animeList.find((anime) => {
   if (anime.title.includes(query)) {
@@ -49,22 +21,5 @@ if (!anime) {
 }
 
 // get fillers
-const fillerPage = await axios.get(`${URL}${anime.path}`);
-const fillers = [];
-
-const $$ = load(fillerPage.data);
-
-$$(".EpisodeList")
-  .find("tbody tr")
-  .each((_, el) => {
-    const episodeType = $$(el).find(".Type span").text().toLowerCase();
-    if (episodeType !== "filler") return;
-
-    const episode = $$(el).find(".Number").text();
-    const title = $$(el).find(".Title a").text();
-
-    fillers.push({ title, episode });
-  });
-
 console.log(`The fillers for ${query} are the ff:`);
-console.log(fillers);
+console.log(await getFillers(anime.path));
