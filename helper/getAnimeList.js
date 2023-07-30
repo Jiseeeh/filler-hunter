199a1ef5594c25ce098ba99e3ec77a5b318abf30
Jiseeeh <johncarlo.camara1@gmail.com>
2023-07-30 22:04:08 +0800

    docs: add function desc

--- helper/getAnimeList.js
+++ helper/getAnimeList.js
@@ -3,6 +3,13 @@ import { load } from "cheerio";
 
 import { URL, AXIOS_OPTIONS } from "../constants/constants.js";
 
+/**
+ * The function `getAnimeList` retrieves a list of anime titles and their corresponding paths from a
+ * webpage.
+ * @returns The function `getAnimeList` returns an array of objects, where each object represents an
+ * anime show. Each object has two properties: `title`, which is a string representing the title of the
+ * show, and `path`, which is a string representing the URL path of the show.
+ */
 export default async function getAnimeList() {
   const showsPage = await axios.get(`${URL}/shows`, AXIOS_OPTIONS);
   const animeList = [];
--- helper/getFillers.js
+++ helper/getFillers.js
@@ -3,6 +3,13 @@ import { load } from "cheerio";
 
 import { URL, AXIOS_OPTIONS } from "../constants/constants.js";
 
+/**
+ * The function `getFillers` retrieves a list of filler episodes from a given path using Axios and
+ * parses the HTML response to extract the episode number and title.
+ * @param path - The `path` parameter is a string that represents the path to the filler page. It is
+ * used to construct the URL for the axios GET request.
+ * @returns an array of objects containing the title and episode number of filler episodes.
+ */
 export default async function getFillers(path) {
   const fillerPage = await axios.get(`${URL}${path}`, AXIOS_OPTIONS);
   const fillers = [];
