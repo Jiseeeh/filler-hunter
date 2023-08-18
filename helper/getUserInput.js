601d55ea4fd60c6da6d769b164bc46061f078d1f
Jiseeeh <johncarlo.camara1@gmail.com>
2023-08-18 21:29:23 +0800

    feat: remove get user input helper fn

--- helper/getUserInput.js
+++
@@ -1,8 +0,0 @@
-import prompt from "prompt-sync";
-import chalk from "chalk";
-
-const input = prompt({ sigint: true });
-
-export default function getUserInput(message) {
-  return input(chalk.greenBright(`>> ${message}`)).toLowerCase();
-}
